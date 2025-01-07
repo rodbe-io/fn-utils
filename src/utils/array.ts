225a857b244e235d5d357819cc7bc214aa9786e4
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-01-07 01:32:45 -0500

    feat: add array utility

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,5 @@
 import { hasValue } from './predicates';
 
 export const purge = <T>(array: Array<T>): Array<T> => array.filter(hasValue);
+
+export const unique = <T>(arr: Array<T>) => [...new Set(arr)];
