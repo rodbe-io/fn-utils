ef877fb6d69c3ca58125b702d410b6ed93c24792
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-01-12 03:11:15 -0500

    feat: update to function

--- src/utils/async.ts
+++ src/utils/async.ts
@@ -1,23 +1,18 @@
 import { isObject } from './predicates';
 
-export const to = async <T, E>(
-  promise: Promise<T>,
+export const to = async <Response, E>(
+  promise: Promise<Response>,
   errInfo?: E
-): Promise<[null, T] | [E extends string ? Error & { errInfo: string } : Error & E, null]> => {
+): Promise<[null, Response] | [Error & E, null]> => {
   try {
     const res = await promise;
 
     return [null, res];
   } catch (err) {
-    if (errInfo && err instanceof Error) {
-      if (typeof errInfo === 'string') {
-        Object.assign(err, { errInfo });
-      }
-      if (isObject(errInfo)) {
-        Object.assign(err, errInfo);
-      }
+    if (isObject(errInfo) && err instanceof Error) {
+      Object.assign(err, errInfo);
     }
 
-    return [err as E extends string ? Error & { errInfo: string } : Error & E, null];
+    return [err as Error & E, null];
   }
 };
